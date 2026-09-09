c024bbfc0f8914aff181212fc06a248e656e78c4
zaim-tech <rehanamahmoud48@gmail.com>
2026-09-09 22:45:24 +0300

    .

--- examples/flet_image_viewer_example/src/main.py
+++ examples/flet_image_viewer_example/src/main.py
@@ -1,30 +1,29 @@
 import flet as ft
 
-from flet_image_viewer import FletImageViewer, FletMultiViewer
-
-import os
+from flet_image_viewer import FletMultiViewer
 
 
 def main(page: ft.Page):
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-
-    base = os.path.dirname(os.path.abspath(__file__))
-    image = os.path.join(base, "assets/test.jpg")
-    image2 = os.path.join(base, "assets/IMG.jpg")
-
-
-    multi = FletMultiViewer(
-        src=["https://avatars.githubusercontent.com/u/201727728?v=4", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBB4LQTn0vRq4ydPLp-uTj_lEUHOHYWUU18JlCq5KuMw&s=10", image, image2],
-        viewer_id="photos",
-        index=0,
-        on_long_press=lambda e: print("long press"),
-        on_double_tap=lambda e: print("double tap")
-        
-
+    page.add(
+        FletMultiViewer(
+            src=[
+                "https://picsum.photos/id/10/900/600",
+                "https://picsum.photos/id/20/900/600",
+                "https://picsum.photos/id/30/900/600",
+            ],
+            viewer_id="photo-gallery",
+            index=0,
+            BoxFit=ft.BoxFit.CONTAIN,
+            overlay=ft.Container(
+                content=ft.Text("Swipe to view more"),
+                bgcolor=ft.Colors.with_opacity(0.75, ft.Colors.BLACK),
+                padding=10,
+            ),
+            on_long_press=lambda event: print("Image long-pressed"),
+            on_double_tap=lambda event: print("Image double-tapped"),
+            expand=True,
+        )
     )
-
-    page.add(multi)
 
 
 ft.run(main)
